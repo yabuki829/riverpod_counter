82ab104c81dcfabbe05abb09cb28a2f765c060dc
yabuki829 <syodai829@icloud.com>
2024-09-08 20:10:26 +0900

    コメントを削除

--- lib/notifier/CounterNotifier.dart
+++ lib/notifier/CounterNotifier.dart
@@ -3,22 +3,20 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'CounterNotifier.g.dart';
 
-// @riverpod アノテーションを使用してプロバイダーを定義
+
 @riverpod
 class CounterNotifier extends _$CounterNotifier {
   @override
   int build() {
-    return 0; // 初期値 0
+    return 0; 
   }
 
-  // カウントアップ処理
+
   void increment() {
-    print("プラスしますぜ兄貴");
-    state++; // 現在の状態に1を加算
+    state++; 
   }
 
-  // リセット処理
   void reset() {
-    state = 0; // 状態をリセット
+    state = 0;
   }
 }
